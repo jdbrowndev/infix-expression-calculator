2e53e6341ea25ca8ad253032c85234cab6760bfe
Jordan Brown <emagdne@gmail.com>
2015-12-22 14:01:30 -0500

    Add test cases for negative numbers.

--- InfixExpressionCalculator.Tests/EvaluateInfixTests.cs
+++ InfixExpressionCalculator.Tests/EvaluateInfixTests.cs
@@ -17,6 +17,11 @@ namespace InfixExpressionCalculator.Tests
         [TestCase("4.47 + 1.02 * 3", 7.53)]
         [TestCase("5.0214 / 6.2033", .8095)]
         [TestCase("1.00000001 * (2.093198 + 5.3232234) - 9.24 / 7", 6.0964)]
+        [TestCase("0 - 100", -100)]
+        [TestCase("8 + (2 - 12)", -2)]
+        [TestCase("8 / (0 - 2)", -4)]
+        [TestCase("8 * (0 - 3)", -24)]
+        [TestCase("8.65468 - 6.65465 - 2.65654", -0.65651)]
         public void Should_Evaluate_An_Infix_Expression_Correctly(string infix, decimal output)
         {
             Assert.That(InfixExpressionCalculator.EvaluateInfix(infix), Is.EqualTo(output).Within(.001));
