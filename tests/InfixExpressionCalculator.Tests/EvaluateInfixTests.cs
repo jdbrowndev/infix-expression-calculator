58d09b179e7d26fd992472fc98dc263bf783b913
Jordan Brown <emagdne@gmail.com>
2022-04-10 19:31:50 -0400

    upgrade to .net 6 / convert nunit to xunit

---
+++ tests/InfixExpressionCalculator.Tests/EvaluateInfixTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace InfixExpressionCalculator.Tests;
+
+public class The_EvaluateInfix_Method
+{
+	[Theory]
+	[InlineData("2 + 2", 4.0)]
+	[InlineData("2", 2.0)]
+	[InlineData("5 + 5 - 2", 8.0)]
+	[InlineData("5 - 2 + 12 / 2 * 3", 1.0)]
+	[InlineData("(5 * 2) + 2", 12.0)]
+	[InlineData("5 * (2 + 2)", 20.0)]
+	[InlineData("(5 * 2) + (5 * 3)", 25.0)]
+	[InlineData("((5 * 2) + (5 * 3))", 25.0)]
+	[InlineData("(5 * (2 + 5) * 3)", 105.0)]
+	[InlineData("4.47 + 1.02 * 3", 7.53)]
+	[InlineData("5.0214 / 6.2033", .8095)]
+	[InlineData("1.00000001 * (2.093198 + 5.3232234) - 9.24 / 7", 6.0964)]
+	[InlineData("0 - 100", -100)]
+	[InlineData("8 + (2 - 12)", -2)]
+	[InlineData("8 / (0 - 2)", -4)]
+	[InlineData("8 * (0 - 3)", -24)]
+	[InlineData("8.65468 - 6.65465 - 2.65654", -0.65651)]
+	public void Should_Evaluate_An_Infix_Expression_Correctly(string infix, decimal output)
+	{
+		Assert.Equal(output, InfixExpressionCalculator.EvaluateInfix(infix), 4);
+	}
+}
---
+++ tests/InfixExpressionCalculator.Tests/EvaluatePostfixTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Xunit;
+
+namespace InfixExpressionCalculator.Tests;
+
+public class The_EvaluatePostfix_Method
+{
+	[Theory]
+	[InlineData("2 2 +", 4.0)]
+	[InlineData("5 7 + 12 - 100 * 1 +", 1.0)]
+	[InlineData("2 9.1 3 + *", 24.2)]
+	public void Should_Evaluate_A_Postfix_Expression_Correctly(string postfix, decimal output)
+	{
+		Assert.Equal(output, InfixExpressionCalculator.EvaluatePostfix(postfix), 4);
+	}
+
+	[Theory]
+	[InlineData("120", 120.0)]
+	public void Should_Return_The_Number_If_A_Single_Number_Is_Given(string postfix, decimal output)
+	{
+		Assert.Equal(output, InfixExpressionCalculator.EvaluatePostfix(postfix), 4);
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData(" ")]
+	[InlineData("\t")]
+	public void Should_Throw_Exception_If_Expression_Is_Empty(string postfix)
+	{
+		var exception = Assert.Throws<ArgumentException>(() => InfixExpressionCalculator.EvaluatePostfix(postfix));
+		Assert.Equal("Expression is empty.", exception.Message);
+	}
+
+	[Theory]
+	[InlineData("2 0 /")]
+	public void Should_Throw_Exception_When_Dividing_By_Zero(string postfix)
+	{
+		Assert.Throws<DivideByZeroException>(() => InfixExpressionCalculator.EvaluatePostfix(postfix));
+	}
+
+	[Theory]
+	[InlineData("+")]
+	[InlineData("+ 2")]
+	[InlineData("2 +")]
+	[InlineData("2 2 + +")]
+	public void Should_Throw_Exception_When_Given_A_Postfix_Expression_With_Too_Many_Operators(string postfix)
+	{
+		var exception = Assert.Throws<Exception>(() => InfixExpressionCalculator.EvaluatePostfix(postfix));
+		Assert.Equal("Too many operators.", exception.Message);
+	}
+
+	[Theory]
+	[InlineData("2 3a +")]
+	public void Should_Throw_Exception_When_Given_A_Postfix_Expression_With_Invalid_Numbers(string postfix)
+	{
+		var exception = Assert.Throws<Exception>(() => InfixExpressionCalculator.EvaluatePostfix(postfix));
+		Assert.Equal("3a is not a valid number.", exception.Message);
+	}
+
+	[Fact]
+	public void Should_Throw_Exception_On_Positive_Decimal_Overflow()
+	{
+		Assert.Throws<OverflowException>(() => InfixExpressionCalculator.EvaluatePostfix(String.Format("{0} 1 +", Decimal.MaxValue)));
+	}
+
+	[Fact]
+	public void Should_Throw_Exception_On_Negative_Decimal_Overflow()
+	{
+		Assert.Throws<OverflowException>(() => InfixExpressionCalculator.EvaluatePostfix(String.Format("{0} 1 -", -Decimal.MaxValue)));
+	}
+}
